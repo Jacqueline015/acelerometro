1a0a5f4f7fb92d86d02d564747cdea1c2af0a47d
Jacqueline015 <jacqueline2iv4@gmail.com>
2022-06-12 20:38:26 -0500

    servidor para aceletrometo y detector de sismos

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+//Se crea la instancia http y se importa la clase http
 const http = require('http');
+//Se crea la instancia express y se importa la clase express
+//Express es un entorno de trabajo para aplicaciones web
 const express = require('express');
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
-emitter.setMaxListeners(0);
 
-//const socketIO = require('socket.io');
+//se crea una instacia app para inicializar express
 const app = express();
 
+//Se crea la instancia server para crear el servidor con la configuracion de app
 const server = http.createServer(app);
-//const io = socketIO.listen(server);
 
+//Se crea la instancia io y se importa la biblioteca de socket.io y se le pasa el servidor 
 const io = require('socket.io')(server);
 
-//app.use(express.static(__dirname + '/public'));
-
+//con el metodo get de la instancia app se crea el servidor
+//Como argumentos se tienen: req=request (peticion) y res= respond(respuesta)
 app.get('/', function(req, res){
+ //se lee el documento acel.html en la carpeta publi
+ //sendFile es un metodo de la instancia res
+ // __dirname tiene la direccionde la carpeta acelerometro
+ //function es una funcion anónima 
   res.sendFile(__dirname + '/public/acel.html');
 });
 
+//Con el metodo listen de la instancia server escucha la peticion del cliente en el  puerto 8080
 server.listen(8080, function(){
  console.log('server listening on port',8080);
 });
@@ -41,14 +47,15 @@ port.pipe(parser)
 
 port.on('open', () => console.log('Port open'))
 
-//parser.on('data',(data) =>   
-//console.log(data);
-//)
+
  
 
 parser.on('data', function(data){
+//Se guardan los datos obtenidos en la variable acel
 	let acel = data;
+//Por medio de la consola se muestran los datos de la variable acel
 	console.log(acel);
+//Con el metodo emit de la instancia io se emiten los datos que se estan recibiendo en un evento llamado acel
 	io.emit('acel',data)
 });
 
